53e22f88ff99dd6f213c3fbbc97a48da758a1b38
Chris Osborne <chris.a.osborne@gmail.com>
2016-06-23 16:15:04 -0500

    minor styling changes

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.use(session({
   cookie: { path: '/', httpOnly: true, secure: false, maxAge: null }
 }));
 
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
 app.use(function(req, res, next) {
   req.session.feed = req.session.feed || [];
   app.locals.feed = req.session.feed;
